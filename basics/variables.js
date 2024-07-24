50fbc2ac51a296ab48d36d2b5c7074151eec69f4
iabhishek023 <114619038+iabhishek023@users.noreply.github.com>
2024-07-24 19:08:48 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const acc=122
+let email="abhishek@gmail.com"
+var accpass=123456
+
+accountcity="gzb"
+
+console.table([acc,email,accpass,accountcity]);
+
+/*
+var not used as scope not defined
+*/
